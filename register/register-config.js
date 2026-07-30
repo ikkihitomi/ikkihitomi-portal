6fe6c4bff455ea46ed007f932e5093caf38c2d58
mjcliant <mitsumasa2223@gmail.com>
2026-07-30 18:15:45 +0900

    Ver1.40-2 完成試験開始

--- register/register-config.js
+++ register/register-config.js
@@ -1,6 +1,6 @@
 /* 一箕地区ポータル Ver1.40-2 設定 */
 window.RESIDENT_REGISTER_CONFIG = {
     liffId: "2010870639-dCVC8ERa",
-    supabaseUrl: "https://aeafysrkqtwgufmhwdb.supabase.co",
+    supabaseUrl: "https://aeafysrkqtwgufmhwwdb.supabase.co",
     functionName: "resident-register"
 };
